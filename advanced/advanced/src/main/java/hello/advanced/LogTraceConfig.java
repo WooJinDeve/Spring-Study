cb691de46445d9d1a98351efc17bc5deef96b187
WooJinDeve <jwj060111@gmail.com>
2022-09-03 16:15:00 +0900

    ThreadLocal

---
+++ advanced/advanced/src/main/java/hello/advanced/LogTraceConfig.java
@@ -0,0 +1,17 @@
+package hello.advanced;
+
+import hello.advanced.trace.logtrace.FieldLogTrace;
+import hello.advanced.trace.logtrace.LogTrace;
+import hello.advanced.trace.logtrace.ThreadLocalLogTrace;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LogTraceConfig {
+
+    @Bean
+    public LogTrace logTrace(){
+        //return new FieldLogTrace();
+        return new ThreadLocalLogTrace();
+    }
+}
