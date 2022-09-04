35ae3ad08a22d8ccf648d031c629d4b196e7108f
WooJinDeve <jwj060111@gmail.com>
2022-09-04 21:02:27 +0900

    BeanPostProcessor

--- proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -1,7 +1,7 @@
 package hello.proxy;
 
-import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV1;
-import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV2;
+import hello.proxy.config.v4_postprocessor.BeanPostProcessorConfig;
+import hello.proxy.config.v5_autoproxy.AutoProxyConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -14,7 +14,9 @@ import org.springframework.context.annotation.Import;
 //@Import({InterfaceProxyConfig.class,ConcreteProxyConfig.class})
 //@Import(DynamicProxyBasicConfig.class)
 //@Import(DynamicProxyFilterConfig.class)
-@Import({ProxyFactoryConfigV1.class,ProxyFactoryConfigV2.class})
+//@Import({ProxyFactoryConfigV1.class,ProxyFactoryConfigV2.class})
+//@Import(BeanPostProcessorConfig.class)
+@Import(AutoProxyConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
 
